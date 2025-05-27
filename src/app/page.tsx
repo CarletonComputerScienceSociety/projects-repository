07729122e84ee507187b22ed2c65fd21526b7989
Matthew MacRae-Bovell <matthewmacraebovell@outlook.com>
2025-05-26 23:51:29 -0400

    Add scss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 import { getAllProjects } from "@/lib/markdown";
 import { Footer, Header, ProjectCard, Search } from "@/components";
 
